1c3cc6a22396688c9c49a3d21a70be9a808912cf
Guilherme Santos <gui044santos@gmail.com>
2026-02-13 22:11:05 -0300

    aa

---
+++ Metros, em cm, e mm.py
@@ -0,0 +1,4 @@
+n = float(input('Digite um valor em metros:'))
+c = n*100
+mm = n*1000
+print('O valor de {} metros, corresponde a {:.0f}cm, e a {:.0f}mm.'.format(n, c, mm))
---
+++ Média de notas.py
@@ -0,0 +1,4 @@
+n1 = float(input('Qual foi sua nota da primeira unidade?'))
+n2 = float(input('Qual foi sua nota da segunda unidade?'))
+m = (n1 + n2)/2
+print('A média das suas duas notas foi {:.2f}!'.format(m))
